180d28aefc5cb2bddfab541971b32cc708d61096
Zubin <me@zubin.org.in>
2023-02-25 20:41:53 +0530

    Minor

--- api/bot.js
+++ api/bot.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Bot, webhookCallback } = require("grammy");
+const { Bot, webhookCallback, HttpError, GrammyError } = require("grammy");
 const wikifeet = require("wikifeet-js");
 
 // Bot
@@ -26,7 +26,7 @@ bot.use(responseTime);
 
 bot.command("start", async (ctx) => {
   await ctx
-    .reply("*Welcome!* ✨ Send the name of a celebrity.", {
+    .reply("*Welcome!* ✨\n_Send the name of a celebrity._", {
       parse_mode: "Markdown",
     })
     .then(() => {
@@ -66,10 +66,10 @@ SELECT * FROM users WHERE userid = ?
 bot.command("help", async (ctx) => {
   await ctx
     .reply(
-      "*@anzubo Project.*\n\nThis bot uses the WikiFeet website.\n_You are required to follow WikiFeet's TOS._",
+      "*@anzubo Project.*\n\n_This bot uses the WikiFeet website.\nSend the name of a celebrity to try it out!_",
       { parse_mode: "Markdown" }
     )
-    .then(() => console.log("Help command message sent."))
+    .then(() => console.log("Help command message sent to", ctx.from.id))
     .catch((error) => console.error(error));
 });
 
@@ -105,37 +105,74 @@ bot.on("msg", async (ctx) => {
       .split(" ")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(" ");
-    await ctx.reply("Searching for " + name);
-    let searchResults = await wikifeet.search(ctx.msg.text);
-    if (searchResults.length === 0) {
+    await ctx.reply(`*Searching for ${name}*`, { parse_mode: "Markdown" });
+    async function searchWikifeet(name) {
+      const searchResults = await wikifeet.search(name);
+      console.log(`${searchResults.length} pics found for ${name}`);
+      return searchResults;
+    }
+    let results = await searchWikifeet(ctx.msg.text);
+    if (results.length === 0) {
       console.log("No results found for:", ctx.msg.text);
       await ctx.reply("No results found for " + name);
       return;
     } else {
-      let query = searchResults[0];
+      let query = results[0];
       let pics = await wikifeet.getImages(query);
-
       var index = [];
       for (let i = 0; i < 3; i++) {
         if (i >= pics.length) {
           console.error("Not enough pics for:", name);
+          await ctx.reply(`Not enough pics for ${name}`);
           return;
         }
         let random = 0 | (pics.length * Math.random());
         index.push(random);
       }
-
       for (let i = 0; i < index.length; i++) {
         await ctx.replyWithPhoto(pics[index[i]]);
       }
     }
   } catch (error) {
-    await ctx.reply("Query: " + ctx.msg.text + " not found!");
-    console.error(error);
-    return;
+    if (error instanceof GrammyError) {
+      console.log(`Error sending message${error.message}`);
+      return;
+    } else {
+      console.log("An error occurred");
+      await ctx.reply(`Query ${name} not found!`, {
+        parse_mode: "Markdown",
+        reply_to_message_id: ctx.msg.message_id,
+      });
+      return;
+    }
   }
 });
 
-// Function
+// Error
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(
+    "Error while handling update",
+    ctx.update.update_id,
+    "\nQuery:",
+    ctx.msg.text
+  );
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+    if (e.description === "Forbidden: bot was blocked by the user") {
+      console.log("Bot was blocked by the user");
+    } else {
+      ctx.reply("An error occurred");
+    }
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+// Run
 
 export default webhookCallback(bot, "http");
